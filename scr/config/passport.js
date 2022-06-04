8197ba8cbb2bdb36cf30dbfaa3d41de822e60574
memosk8 <memolopez9363@gmail.com>
2022-06-04 12:58:28 -0500

    refactoring code

--- scr/config/passport.js
+++ scr/config/passport.js
@@ -1,37 +1,23 @@
-//estrategia para autentificar al usuario
-
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/Users');
 
 //Estrategia de autenticacion
-passport.use(new LocalStrategy({
-    //renombrar el dato username 
-    usernameField:'email'
-
-}, async (email, password, done) =>{
-    const user = await User.findOne({email:email});
-    if(!user){
-        return done(null, false, { message:"El usuario no existe" });
+passport.use(new LocalStrategy(
+    {usernameField:'email'},
+    async (email, password, done) => {
+        const user = await User.findOne({email:email});
+        if(!user) return done(null, false, { message:"El usuario no existe" })
+        else {const match= await user.matchPassword(password)}
+        if(match) return done(null, user);
+        else return done(null, false, {message:"contraseña incorrecta"})
     }
-    else{
-        const match= await user.matchPassword(password);
-        if(match){
-            return done(null, user);
-        } else{
-            return done(null, false, {message:"contraseña incorrecta"});
-        }
-    }
-}));
+));
 
 //Serializar al usuario
-passport.serializeUser((user, done)=>{
-    done(null, user.id);
-})
+passport.serializeUser((user, done) => done(null, user.id))
 
 //Deseralizar a los usuarios
 passport.deserializeUser((id, done)=>{
-    User.findById(id,(err, user)=>{
-        done(err,user);
-    })
+    User.findById(id,(err, user) => done(err,user) )
 })
