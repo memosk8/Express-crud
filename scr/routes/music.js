955084cf20c997d66c707524a1292a4a8eea8d8c
memosk8 <memolopez9363@gmail.com>
2022-06-27 15:08:31 -0500

    working on getting an arr from req obj to update info on db accordingly

--- scr/routes/music.js
+++ scr/routes/music.js
@@ -5,18 +5,24 @@ const Category = require('../models/Categories');
 router.get('/music', isAuthenticated, async (req, res) => {
    console.log(`\n Solicitud en ${req.url}`)
    const categories = await Category.find({}).lean();
-   categories.forEach(section => {
-      console.log(section)
-   });
-   res.render('music/music', { categories});
+   res.render('music/music', { categories });
 });
 
 router.get('/music/edit/:id', isAuthenticated, async (req, res) => {
    //consulta a la base de datos
-   const categories = await Category.findById(req.params.id).sort(-1).lean();
-   categories.forEach(category => {
-      category.total = category.content.length
-   });
+   const category = await Category.findById(req.params.id).lean();
    res.render('music/edit-category', { category })
 });
+
+//actualizar las categorias en la base de datos
+router.put('/music/edit-category/:id', isAuthenticated, async (req, res) => {
+   const { title } = req.body;
+   const content = Object.entries(req.body)
+   content.slice(0,1)
+   console.log(content)
+   await Category.findByIdAndUpdate(req.params.id, { title });
+   req.flash('success_msg', 'Editado correctamente');
+   res.redirect('/music');
+});
+
 module.exports = router;
