d137810b708a1db8f6986cf0ceb430d3312ef06b
memosk8 <memolopez9363@gmail.com>
2022-06-27 00:53:08 -0500

    fetch and list tunein music categories from db

--- scr/routes/music.js
+++ scr/routes/music.js
@@ -4,8 +4,19 @@ const Category = require('../models/Categories');
 
 router.get('/music', isAuthenticated, async (req, res) => {
    console.log(`\n Solicitud en ${req.url}`)
-   const categories = await Category.find({});
-   console.log(categories.length)
-   res.render('music', { categories});
+   const categories = await Category.find({}).lean();
+   categories.forEach(section => {
+      console.log(section)
+   });
+   res.render('music/music', { categories});
+});
+
+router.get('/music/edit/:id', isAuthenticated, async (req, res) => {
+   //consulta a la base de datos
+   const categories = await Category.findById(req.params.id).sort(-1).lean();
+   categories.forEach(category => {
+      category.total = category.content.length
+   });
+   res.render('music/edit-category', { category })
 });
 module.exports = router;
