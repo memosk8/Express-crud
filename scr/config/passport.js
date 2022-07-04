fd417c02bded70af3de5d52e0632e8d38e040409
memosk8 <memolopez9363@gmail.com>
2022-07-04 12:08:12 -0500

    + session var to check if logged used is admin

--- scr/config/passport.js
+++ scr/config/passport.js
@@ -1,12 +1,14 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/Users');
+const session = require('express-session');
 
 //Estrategia de autenticacion
 passport.use(new LocalStrategy(
 	{ usernameField: 'email' },
 	async (email, password, done) => {
 		const user = await User.findOne({ email: email })
+		session.admin = user.admin;
 		if (!user) return done(null, false, { message: "El usuario no existe" })
 		else {
 			const match = await user.matchPassword(password)
