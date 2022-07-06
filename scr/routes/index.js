7ae0e11a27617d83e6af3ea2b225c159b0d17ce0
memosk8 <memolopez9363@gmail.com>
2022-07-06 13:29:36 -0500

    + showing usr adm and usr name

--- scr/routes/index.js
+++ scr/routes/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
+const session = require('express-session');
 
-router.get('/', (req, res) => res.render('index'));
+router.get('/', (req, res) => {
+  const admin = session.admin
+  const username = session.username
+  res.render('index', { admin, username })
+});
+
 router.get('/about', (req, res) => res.render('about'));
 
 module.exports = router;
